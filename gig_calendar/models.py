164463ca480513fba245cd680d227beeac70ed14
Corey <coreycr@icloud.com>
2019-04-04 11:55:08 -0500

    Created Gig and Venue models.

--- gig_calendar/models.py
+++ gig_calendar/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Gig(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    venue = models.ForeignKey(Venue)
+    description = models.CharField(max_length=1000)
+    info_URL = models.URLField()
+
+class Venue(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
