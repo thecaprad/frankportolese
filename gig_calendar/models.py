459771034a0c9d61cff370aa0422bc53a587aa9d
Corey <coreycr@icloud.com>
2019-04-26 13:52:06 -0500

    Registered Venue and Gig models.

--- gig_calendar/models.py
+++ gig_calendar/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Venue(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 class Gig(models.Model):
     date = models.DateField()
