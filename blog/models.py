328e8807643b7bd344ceb8a33779b9ad5d430a2d
Corey <coreycr@icloud.com>
2019-05-01 16:28:55 -0500

    Simplified BlogPost to save just the publish date, rather than date and time. The time field seems unnecesary.

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,15 @@
+import datetime
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 class BlogPost(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    published_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateField(blank=True, null=True)
     
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date = datetime.date.today()
         self.save()
     
     def __str__(self):
