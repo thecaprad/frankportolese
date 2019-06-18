483454e06db13b0fd06461b9fc358c258975ace5
Corey <coreycr@icloud.com>
2019-06-18 12:59:51 -0500

    Created gig_list template and updated the web page to simply spit out the gigs. No styling.

--- gig_calendar/views.py
+++ gig_calendar/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Venue, Gig
 
 def gig_list(request):
-    return render(request, 'gig_calendar/gig_list.html', {})
+    gigs = Gig.objects.all().order_by('date')
+    return render(request, 'gig_calendar/gig_list.html', {'gigs': gigs})
