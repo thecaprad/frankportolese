973feeac8e2016e2de655a41a9e25dbdd67dd1a0
corey <coreycr@icloud.com>
2019-07-04 09:48:48 +0200

    Increated text area size of description field on gigs. Made description and info_url attributes not required as not all gigs will have them.

--- gig_calendar/models.py
+++ gig_calendar/models.py
@@ -12,8 +12,14 @@ class Gig(models.Model):
     date = models.DateField()
     time = models.TimeField()
     venue = models.ForeignKey(Venue, on_delete=models.DO_NOTHING)
-    description = models.CharField(max_length=1000)
-    info_URL = models.URLField()
+    description = models.TextField(
+        blank=True,
+        null=False,
+    )
+    info_URL = models.URLField(
+        blank=True,
+        null=False
+    )
     
     def __str__(self): # 'The Green Mill on 04/28/2019'
         return "{} on {}".format(self.venue, self.date)
