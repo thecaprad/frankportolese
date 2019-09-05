52c5cc5b8c11f0cc5f70a8a79e0d98d899bfa856
corey <coreycr@icloud.com>
2019-09-05 17:11:55 -0500

    Registered BlogPost model to admin.py.

--- gig_calendar/admin.py
+++ gig_calendar/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Venue, Gig
+from .models import Venue, Gig, BlogPost
 
 # Register your models here.
 
 admin.site.register(Venue)
 admin.site.register(Gig)
+admin.site.register(BlogPost)
